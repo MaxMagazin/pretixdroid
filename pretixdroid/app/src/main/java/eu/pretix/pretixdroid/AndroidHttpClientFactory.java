8509491138e3043fa4526b3ede44a3bcedb07dd2
Maxim Yastremsky <yastremsky@silpion.de>
2018-09-03 20:13:05 +0200

    set connection, read/write timeouts for Andorid http client

--- pretixdroid/app/src/main/java/eu/pretix/pretixdroid/AndroidHttpClientFactory.java
+++ pretixdroid/app/src/main/java/eu/pretix/pretixdroid/AndroidHttpClientFactory.java
@@ -2,18 +2,29 @@ package eu.pretix.pretixdroid;
 
 import com.facebook.stetho.okhttp3.StethoInterceptor;
 
+import java.util.concurrent.TimeUnit;
+
 import eu.pretix.libpretixsync.api.HttpClientFactory;
 import okhttp3.OkHttpClient;
 
 public class AndroidHttpClientFactory implements HttpClientFactory {
+
+    private static final int TIMEOUT = 30;
+
     @Override
     public OkHttpClient buildClient() {
         if (BuildConfig.DEBUG) {
             return new OkHttpClient.Builder()
                     .addNetworkInterceptor(new StethoInterceptor())
+                    .connectTimeout(TIMEOUT, TimeUnit.SECONDS) // connect timeout
+                    .readTimeout(TIMEOUT, TimeUnit.SECONDS)    // socket timeout
+                    .writeTimeout(TIMEOUT / 2, TimeUnit.SECONDS)
                     .build();
         } else {
             return new OkHttpClient.Builder()
+                    .connectTimeout(TIMEOUT, TimeUnit.SECONDS) // connect timeout
+                    .readTimeout(TIMEOUT, TimeUnit.SECONDS)    // socket timeout
+                    .writeTimeout(TIMEOUT / 2, TimeUnit.SECONDS)
                     .build();
         }
     }
